6c5c6c07b2376a6080fd2b90cd1e05770cdce0a1
marysonthego <marysonthego@gmail.com>
2022-04-25 11:53:33 -0600

    initial

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <input type="checkbox" id="todo-item" />
+  <label for="todo-item">{{label}}</label>
+</template>
+
+<script>
+  export default {
+    props: {
+      label:{required: true, default: "Learn Vue", type: String},
+      done:{default: false, type: Boolean}
+    }
+  };
+</script>
+
